21f2e47af4435ef77d572e87737909ef5b858e75
ncroddy <31410945+ncroddy@users.noreply.github.com>
2017-09-27 22:29:32 -0500

    updated

--- src/test/java/edu/luc/cs/cs271/lab2/TestSearch.java
+++ src/test/java/edu/luc/cs/cs271/lab2/TestSearch.java
@@ -14,7 +14,7 @@ public class TestSearch {
     return array;
   }
 
-  // TODO makeListFixture
+  // DONE DRY makeListFixture 
 
   @Test
   public void testFindPositionArray0() {
@@ -34,9 +34,65 @@ public class TestSearch {
     assertFalse(Search.findTeamPosition(arr, "Team 11").isPresent());
   }
   
-  // TODO: testFindPositionList0, 10s, 10f
+  // DONE DRY: testFindPositionList0, 10s, 10f
+  @Test
+     public void testFindPositionList0() {
+         final List<Team> list = makeListFixture(0);
+         assertFalse(Search.findTeamPosition(list, "Team 2").isPresent());
+     }
+ 
+     @Test
+     public void testFindPositionList10s() {
+         final List<Team> list = makeListFixture(10);
+         assertTrue(Search.findTeamPosition(list, "Team 7").isPresent());
+     }
+ 
+     @Test
+     public void testFindPositionList10f() {
+         final List<Team> list = makeListFixture(10);
+         assertFalse(
   
-  // TODO: testFindMinFundingArray for several sizes and scenarios
+  // DONE DRY: testFindMinFundingArray for several sizes and scenarios 
+  @Test
+     public void testFindMinFundingArray200() {
+         final Team[] arr = makeArrayFixture(3);
+         final int minFunding = 400;
+         assertEquals(Optional.of(2), Search.findTeamMinFunding(arr, minFunding));
+     }
+ 
+     @Test
+     public void testFindMinFundingArray900() {
+         final Team[] arr = makeArrayFixture(3);
+         final int minFunding = 1000;
+         assertEquals(Optional.empty(), Search.findTeamMinFunding(arr, minFunding));
+     }
+ 
+     @Test
+     public void testFindMinFundingArray400() {
+         final Team[] arr = makeArrayFixture(7);
+         final int minFunding = 420;
+         assertEquals(Optional.of(4), Search.findTeamMinFunding(arr, minFunding));
+     }
+  
 
-  // TODO: testFindMinFundingArrayFast for several sizes and scenarios
-}
+  // DONE DRY: testFindMinFundingArrayFast for several sizes and scenarios 
+    @Test
+     public void testFindMinFundingArrayFast400() {
+         final Team[] arr = makeArrayFixture(5);
+         final int minFunding = 420;
+         assertEquals(Optional.of(4), Search.findTeamMinFundingFast(arr, minFunding));
+     }
+ 
+     @Test
+     public void testFindMinFundingArrayFast700() {
+         final Team[] arr = makeArrayFixture(2);
+         final int minFunding = 690;
+         assertEquals(Optional.empty(), Search.findTeamMinFundingFast(arr, minFunding));
+     }
+ 
+     @Test
+     public void testFindMinFundingArrayFast100() {
+         final Team[] arr = makeArrayFixture(3);
+         final int minFunding = 200;
+         assertEquals(Optional.of(1), Search.findTeamMinFundingFast(arr, minFunding));
+     }
