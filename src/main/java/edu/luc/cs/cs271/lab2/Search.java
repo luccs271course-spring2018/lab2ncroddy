9131fd5f33435bb10aef414c04cbecdac33b2696
ncroddy <31410945+ncroddy@users.noreply.github.com>
2017-09-27 22:21:09 -0500

    Update Search.java

--- src/main/java/edu/luc/cs/cs271/lab2/Search.java
+++ src/main/java/edu/luc/cs/cs271/lab2/Search.java
@@ -23,7 +23,11 @@ public class Search {
 
   /** Looks for the position of the named team in a list. */
   public static Optional<Integer> findTeamPosition(final List<Team> list, final String key) {
-    // TODO complete this method
+    // DONE complete this method
+    final int size = list.size();
+    for (int i = 0; i < size; i++){
+      if (list.get(i).getName().equals(key)) return Optional.of(i);
+    }
     return Optional.empty();
   }
   
@@ -34,33 +38,71 @@ public class Search {
    * @post arr[result].funding >= minFunding && for all 0 <= i < result : arr[i].funding < minFunding
    */
   public static Optional<Integer> findTeamMinFunding(final Team[] arr, final int minFunding) {
-    // TODO complete this method
-    return Optional.empty();
+    
+    final int size = arr.length;
+    Optional<Integer> currentMinPos = Optional.empty();
+    int currentMinFunding;
+    
+    if (arr.length >= 1){ //in case we have an array of length 0
+      currentMinFunding = arr[arr.length - 1].getFunding();  //because we know the highest value is the last team
+    }
+    else return currentMinPos;
+    
+    for (int i = 0; i < size; i++){
+      if(arr[i].getFunding() <= currentMinFunding && arr[i].getFunding() >= minFunding ){
+        currentMinFunding = arr[i].getFunding();
+        currentMinPos = Optional.of(i);
+      }
+    }
+    // DONE DRY complete this method
+    return currentMinPos;
   }
   
   /** 
    * Looks for the position of the poorest team that has at least 
    * the specified funding level. 
-   * Uses binary search: Initially consider the entire index range, 
+   * Uses binary search: Initially search the entire array, 
    * then repeatedly eliminate the wrong half of the array until 
    * zero or one items are left.
    * @pre arr is sorted
    * @post arr[result].funding >= minFunding && for all 0 <= i < result : arr[i].funding < minFunding
    */
   public static Optional<Integer> findTeamMinFundingFast(final Team[] arr, final int minFunding) {
-    // TODO complete this method
+    // DONE DRY complete this method
     // Gets the array size
     final int size = arr.length;
-    // Initially consider the entire index range of the array
+    Optional<Integer> currentMinPos = Optional.empty();
+    if(arr.length < 1) return currentMinPos;
+    // Initially search the entire array 
     int low = 0;
     int high = size - 1;
     // Keep going as long as there is more than one item to be checked
-    // Eliminate the wrong half of the array
+    while ( low <= high){
+      final int mid = (low + high)/2 + (low +high)%2;
+      
+      if( low == high && arr[mid].getFunding() >= minFunding){
+          currentMinPos = Optional.of(mid);
+          return currentMinPos;
+        }
+      else if(low == high){
+        return currentMinPos;
+      }
+        //if(mid == 0) return Optional.empty();
+      // Eliminate the wrong half of the array
+      if ( arr[mid].getFunding() < minFunding){
+        low = mid + 1;
+      }
+      
+      else if(arr[mid-1].getFunding() >= minFunding){
+        high = mid - 1;
+      }
+      else if (arr[mid].getFunding() >= minFunding && arr[mid-1].getFunding() < minFunding){
+        low = mid;
+        high = mid;
+      }
     // Return current item only if it meets the condition!
-    if (low <= high && arr[low].getFunding() >= minFunding) {
-      return Optional.of(low);
-    } else {
-      return Optional.empty();
+     
     }
+    return currentMinPos;
   }
 }
